25eb857de5809e41326718280fc7d57cff014779
Isaac <63469131+isaacsbox@users.noreply.github.com>
2022-11-19 20:34:14 +0000

    small commit

--- game_draft.py
+++ game_draft.py
@@ -8,7 +8,9 @@ def scale_image(img, factor):
     return pygame.transform.scale(img, size)
 
 def blit_rotate_center(win, image, top_left, angle):
-    rotated
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft = top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
 
 
 FPS = 60
