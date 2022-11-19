d94e1b08981788a33a5e81f8433024d86b3e7372
Isaac <63469131+isaacsbox@users.noreply.github.com>
2022-11-19 20:21:39 +0000

    First template

---
+++ game_draft.py
@@ -0,0 +1,57 @@
+# using the useful: https://www.youtube.com/watch?v=L3ktUWfAMPg
+
+import pygame
+pygame.init()
+
+def scale_image(img, factor):
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated
+
+
+FPS = 60
+
+WIDTH, HEIGHT = 500, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+CAR = scale_image(pygame.image.load('car.png'), 0.05)
+GRASS = scale_image(pygame.image.load('grass.png'), 2.5)
+
+class Car:
+    def __init__(self, max_vel, rotation_vel):
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+    
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel 
+
+clock = pygame.time.Clock()
+pygame.display.set_mode((WIDTH, HEIGHT))
+
+FPS = 60
+clock = pygame.time.Clock()
+
+run = True
+while run:
+
+    clock.tick(FPS)
+
+    WIN.blit(GRASS, (0,0))
+    WIN.blit(CAR, (0,0))
+    # put track here 
+
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+pygame.quit()
